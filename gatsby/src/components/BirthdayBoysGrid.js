9ea683ebebf1c7e9a50a7d9d15ee5bf251541a41
Alex Kräuchi <alex@smallstack.com>
2021-02-28 15:57:23 +0100

    Improve birthdayboy cards

--- gatsby/src/components/BirthdayBoysGrid.js
+++ gatsby/src/components/BirthdayBoysGrid.js
@@ -6,7 +6,8 @@ const BirthdayBoysGridStyles = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
   gap: 4rem;
-  grid-auto-rows: 300px auto auto auto;
+  grid-auto-rows: auto 300px auto auto;
+  margin-top: 2rem;
 `;
 
 export default function BirthdayBoysGrid({ birthdayBoys }) {
@@ -22,15 +23,20 @@ export default function BirthdayBoysGrid({ birthdayBoys }) {
 const BirthdayBoyCardStyles = styled.div`
   display: grid;
   @supports not (grid-template-rows: subgrid) {
-    grid-template-rows: 1fr, auto, auto, auto
+    grid-template-rows: auto, 1fr, auto, auto
   }
   grid-template-rows: subgrid; /* Row sizing from BirthdayBoysGridStyles */
   grid-row: span 4;
   grid-gap: 1rem;
   h2 {
-    text-align: left;
+    transform: rotate(-0.5deg);
+    text-align: center;
+    margin-bottom: -2rem;
+    z-index: 4;
+    background-color: var(--orange);
+    padding: 0 2px 2px 2px;
   }
-  h2, p {
+  p {
     margin: 0;
   }
 `;
@@ -39,8 +45,8 @@ function BirthdayBoyCard({ birthdayBoy }) {
   return (
     <>
       <BirthdayBoyCardStyles>
-          <Img fluid={birthdayBoy.image.asset.fluid} alt={birthdayBoy.name}/>
           <h2>{birthdayBoy.name}</h2>
+          <Img fluid={birthdayBoy.image.asset.fluid} alt={birthdayBoy.name}/>
           <p>{birthdayBoy.birthday}</p>
           <p>{birthdayBoy.description}</p>
       </BirthdayBoyCardStyles>
