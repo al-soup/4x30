ac9877b71d7cc99b1859af616601ef8962bb4566
Alex Kräuchi <alex@smallstack.com>
2021-03-08 01:16:24 +0100

    Add useForm helper function

---
+++ gatsby/src/utils/useForm.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function useForm(defaults) {
+  const [values, setValues] = useState(defaults);
+
+  function updateValue(e) {
+    let { value } = e.target;
+    setValues({
+      ...values,
+      [e.target.name]: value
+    });
+  }
+
+  return { values, updateValue };
+}
