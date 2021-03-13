7aeb19e4520363ee78bb4d40f9068d3cfe09f1f8
Alex Kräuchi <alex@smallstack.com>
2021-03-13 15:09:21 +0100

    Fix ssr fn; Change responsive layout

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -1,3 +1,6 @@
-import wrapPageElement from "./gatsby-browser";
+import React from "react";
+import Layout from "./src/components/Layout";
 
-export { wrapPageElement };
+export function wrapPageElement({ element, props }) {
+  return <Layout { ...props }>{ element }</Layout>
+}
